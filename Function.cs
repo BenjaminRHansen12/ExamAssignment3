f7ec6eec4523368d0596b70c5cdce0b7636c49a9
BenjaminRHansen12 <benjaminrudi1@gmail.com>
2024-03-19 03:05:10 +0100

    Soon done with part 1

--- Function.cs
+++ Function.cs
@@ -1,1 +1,40 @@
-Console.WriteLine("hei;)");
+﻿double testNumber = 5;
+double testNumberThing = AreaOfACircle(testNumber);
+Console.WriteLine($"From {testNumber} to {testNumberThing}");
+
+//Functions
+//Square of a number
+static int SquareOfANumber(int number)
+{
+    return number * number;
+}
+
+//From inches to mm
+static double InchesToMM(double inches)
+{
+    return inches * 25.4;
+}
+
+//Root of a number
+//static double RootOfANumber(double number) // do it later
+//{
+    //return;
+//}
+
+//Cube of a number
+static int CubeOfANumber(int number)
+{
+    return number * number * number;
+}
+
+//Area of a circle given radius
+static double AreaOfACircle(double radius)
+{
+    return 3.14 * (radius * radius);
+}
+
+//Greeting given a name
+static string GreetingSomeone(string name)
+{
+    return $"Greetings, {name}";
+}
